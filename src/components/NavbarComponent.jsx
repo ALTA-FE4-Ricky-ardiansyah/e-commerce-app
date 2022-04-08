38bd6c990557ebffe353deeb1aafdde6559baccc
ricky ardiansyah <rickyrichk@gmail.com>
2022-04-08 14:12:13 +0700

    fix login and signup

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -3,6 +3,17 @@ import cart from "../assets/shopping-cart(1).png";
 import userIcon from "../assets/user.png";
 
 export default function NavbarComponent(props) {
+  const Auth = () => {
+    // ambil localstorage
+    const data = localStorage.getItem("user-info");
+    // cek apakah ada localStorage
+    if (data) {
+      // convert json yang awalnya bentuk string jadi objek
+      const json = JSON.parse(data);
+      // ambil namanya
+      // return <span>Hallo {json.User.username}</span>;
+    }
+  };
   return (
     <nav className=" block px-6 py-4 shadow-lg bg-slate-100 w-full">
       <div className="flex justify-between">
@@ -18,7 +29,10 @@ export default function NavbarComponent(props) {
           <a href="/cart">
             <img className="w-7" src={cart} alt="shopcart" />
           </a>
-          <img className="w-7 h-7  mx-6" src={userIcon} alt="userIcon" />
+          {Auth()}
+          <a href="/sellProduct">
+            <img className="w-7 h-7  mx-6" src={userIcon} alt="userIcon" />
+          </a>
         </div>
       </div>
     </nav>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
 import MenuComponent from "../components/MenuComponent";
 import NavbarComponent from "../components/NavbarComponent";
-import banner from "../assets/banner.jpg";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [name, setName] = useState({});
+  const [password, setPassword] = useState({});
+  const [email, setEmail] = useState({});
+
+  // for login
+  const [passwordLogin, setPasswordLogin] = useState(null);
+  const [emailLogin, setEmailLogin] = useState(null);
+
+  const navigate = useNavigate();
+
+  async function signup() {
+    let item = { name, password, email };
+    console.warn(name, password, email);
+
+    let result = await fetch("http://34.238.121.143:8000/api/users", {
+      method: "POST",
+      body: JSON.stringify(item),
+      headers: { "Content-Type": "text/plain", Accept: "*/*" },
+    });
+    result = await result.json();
+    localStorage.setItem("user-info", JSON.stringify(result));
+    navigate("/home");
+    alert("halo");
+  }
+
+  async function signIn() {
+    let result = await fetch("http://34.238.121.143:8000/api/auth", {
+      method: "POST",
+      body: {
+        username: emailLogin,
+        password: passwordLogin,
+      },
+      headers: { "Content-Type": "text/plain", Accept: "*/*" },
+    });
+    result = await result.json();
+    console.log(result.data);
+    localStorage.setItem("user-info", JSON.stringify(result.data));
+    navigate("/home");
+  }
+
   return (
     <div className="bg-slate-100 h-screen">
       <NavbarComponent />
-      <div class="flex justify-center mt-10">
-        <div class="block p-6 rounded-lg shadow-lg bg-white w-9/12">
-          <div class="flex justify-center">
+      <div className="flex justify-center mt-10">
+        <div className="block p-6 rounded-lg shadow-lg bg-white w-9/12">
+          <div className="flex justify-center">
             <h5>Sign Up</h5>
           </div>
-          <form class="mt-6">
-            <div class="form-group mb-6">
+          <form className="mt-6">
+            <div className="form-group mb-6">
               <input
-                type=""
-                class="form-control
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control
                 block
                 w-full
                 px-3
@@ -37,10 +79,12 @@ export default function Login() {
                 placeholder="Username"
               ></input>
             </div>
-            <div class="form-group mb-6">
+            <div className="form-group mb-6">
               <input
-                type=""
-                class="form-control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control
                 block
                 w-full
                 px-3
@@ -60,10 +104,12 @@ export default function Login() {
                 placeholder="Password"
               ></input>
             </div>
-            <div class="form-group mb-6">
+            <div className="form-group mb-6">
               <input
-                type=""
-                class="form-control
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control
                 block
                 w-full
                 px-3
@@ -84,10 +130,11 @@ export default function Login() {
               ></input>
             </div>
           </form>
-          <div class="flex justify-end">
+          <div className="flex justify-end">
             <button
+              onClick={() => signup()}
               type="submit"
-              class="
+              className="
             px-6
             py-2.5
             bg-orange-600
@@ -110,19 +157,20 @@ export default function Login() {
           </div>
         </div>
       </div>
-      <div class="flex justify-center mt-6">
+      <div className="flex justify-center mt-6">
         <h5>or already have an acount?</h5>
       </div>
-      <div class="flex justify-center mt-10">
-        <div class="block p-6 rounded-lg shadow-lg bg-white w-9/12">
-          <div class="flex justify-center">
+      <div className="flex justify-center mt-10">
+        <div className="block p-6 rounded-lg shadow-lg bg-white w-9/12">
+          <div className="flex justify-center">
             <h5>Sign In</h5>
           </div>
-          <form class="mt-6">
-            <div class="form-group mb-6">
+          <form className="mt-6">
+            <div className="form-group mb-6">
               <input
+                onChange={(e) => setEmailLogin(e.target.value)}
                 type=""
-                class="form-control
+                className="form-control
                 block
                 w-full
                 px-3
@@ -142,10 +190,11 @@ export default function Login() {
                 placeholder="Username"
               ></input>
             </div>
-            <div class="form-group mb-6">
+            <div className="form-group mb-6">
               <input
+                onChange={(e) => setPasswordLogin(e.target.value)}
                 type=""
-                class="form-control
+                className="form-control
                 block
                 w-full
                 px-3
@@ -166,10 +215,13 @@ export default function Login() {
               ></input>
             </div>
           </form>
-          <div class="flex justify-end">
+          <div className="flex justify-end">
             <button
               type="submit"
-              class="
+              onClick={() => {
+                signIn();
+              }}
+              className="
             px-6
             py-2.5
             bg-orange-600
